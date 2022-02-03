86cb4e1c824ff6fb4320a73da19c6e4bc030823d
Radosław Adamczyk <greatpolandweb@gmail.com>
2022-02-03 11:16:30 +0100

    add image poster inside poster image odf about section

--- src/components/atoms/HomePage/About/Poster.tsx
+++ src/components/atoms/HomePage/About/Poster.tsx
@@ -1,10 +1,53 @@
+import { graphql, useStaticQuery } from 'gatsby';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import React from 'react';
 import { path } from '../../../../types/types';
+import PosterWrapper from './styles/PosterWrapper';
 const Poster = ({poster}:{poster:path}) => {
+    const {allImageSharp} = useStaticQuery(graphql`
+    {
+      allImageSharp(filter: {id: {eq: "d3486549-24dc-5fb7-98fe-6f4640b6d830"}}) {
+        edges {
+          node {
+            id
+            gatsbyImageData(
+              layout: CONSTRAINED
+              placeholder: TRACED_SVG
+              quality: 8
+              formats: JPG
+            )
+          }
+        }
+      }
+    }
+  `)
+  const imagePath = allImageSharp.edges[0].node.gatsbyImageData;
+  console.log(imagePath);
+  const image = getImage(imagePath);
     return(
-        <figure>
-            <img src={poster}/>
-        </figure>
+        <PosterWrapper>
+            <GatsbyImage 
+                alt='Agromar about image'
+                image={image} 
+                style={{
+                    height:'100%',
+                    objectFit:'cover',
+                    objectPosition:'center',
+                    width:'100%'
+                }}
+            />
+        </PosterWrapper>
     )
 }
 export default Poster;
+// d3486549-24dc-5fb7-98fe-6f4640b6d830
+// query MyQuery {
+//     allSitePlugin {
+//       edges {
+//         node {
+//           id
+//         }
+//       }
+//     }
+//   }
+  
--- src/components/atoms/HomePage/About/styles/PosterWrapper.js
+++ src/components/atoms/HomePage/About/styles/PosterWrapper.js
@@ -1,1 +1,8 @@
 import styled from 'styled-components';
+const PosterWrapper = styled.figure`
+    background-color:pink;
+    height:100%;
+    max-height:45rem;
+    width:100%;
+`
+export default PosterWrapper;
